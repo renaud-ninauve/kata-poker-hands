cc0d792fb6b319ccb202de56015f37832b1d59d3
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-06-11 15:55:28 +0200

    [red][red] straightflush beats flush

--- src/test/java/fr/ninauve/renaud/kata/pokerhands/domain/model/HandTest.java
+++ src/test/java/fr/ninauve/renaud/kata/pokerhands/domain/model/HandTest.java
@@ -4,9 +4,9 @@ import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Suit.CLUBS;
 import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Suit.DIAMONDS;
 import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Suit.HEARTS;
 import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Suit.SPADES;
+import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.EIGHT;
 import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.FIVE;
 import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.FOUR;
-import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.HEIGHT;
 import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.SIX;
 import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.TEN;
 import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.THREE;
@@ -21,16 +21,15 @@ class HandTest {
   void equals_when_same_values_different_distinctSuits() {
 
     final Hand hand1 =
-        Hand.of(
-            TWO.of(DIAMONDS), FOUR.of(HEARTS), SIX.of(SPADES), HEIGHT.of(CLUBS), TEN.of(SPADES));
+        Hand.of(TWO.of(DIAMONDS), FOUR.of(HEARTS), SIX.of(SPADES), EIGHT.of(CLUBS), TEN.of(SPADES));
 
     final Hand hand2 =
         Hand.of(
-            TWO.of(HEARTS), FOUR.of(SPADES), SIX.of(HEARTS), HEIGHT.of(SPADES), TEN.of(DIAMONDS));
+            TWO.of(HEARTS), FOUR.of(SPADES), SIX.of(HEARTS), EIGHT.of(SPADES), TEN.of(DIAMONDS));
 
     final int actual = hand1.compareTo(hand2);
 
-    assertThat(actual).isEqualTo(0);
+    assertThat(actual).isZero();
   }
 
   @Test
@@ -45,10 +44,28 @@ class HandTest {
             SIX.of(DIAMONDS));
 
     final Hand highCard =
-        Hand.of(
-            TWO.of(DIAMONDS), FOUR.of(HEARTS), SIX.of(SPADES), HEIGHT.of(CLUBS), TEN.of(SPADES));
+        Hand.of(TWO.of(DIAMONDS), FOUR.of(HEARTS), SIX.of(SPADES), EIGHT.of(CLUBS), TEN.of(SPADES));
 
     final int actual = straightFlush.compareTo(highCard);
+
+    assertThat(actual).isEqualTo(1);
+  }
+
+  @Test
+  void straightflush_beats_flush() {
+
+    final Hand straightFlush =
+        Hand.of(
+            TWO.of(DIAMONDS),
+            THREE.of(DIAMONDS),
+            FOUR.of(DIAMONDS),
+            FIVE.of(DIAMONDS),
+            SIX.of(DIAMONDS));
+
+    final Hand flush =
+        Hand.of(TWO.of(CLUBS), FOUR.of(CLUBS), SIX.of(CLUBS), EIGHT.of(CLUBS), TEN.of(CLUBS));
+
+    final int actual = straightFlush.compareTo(flush);
 
     assertThat(actual).isEqualTo(1);
   }
