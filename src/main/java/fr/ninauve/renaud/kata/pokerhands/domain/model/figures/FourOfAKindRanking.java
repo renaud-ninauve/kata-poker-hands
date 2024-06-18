ab1acf3adbc7d7ef403678270cc79a5ded649e8e
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-06-18 17:21:24 +0200

    [red][red][green] detect four

--- src/main/java/fr/ninauve/renaud/kata/pokerhands/domain/model/figures/FourOfAKindRanking.java
+++ src/main/java/fr/ninauve/renaud/kata/pokerhands/domain/model/figures/FourOfAKindRanking.java
@@ -1,13 +1,20 @@
 package fr.ninauve.renaud.kata.pokerhands.domain.model.figures;
 
+import fr.ninauve.renaud.kata.pokerhands.domain.model.Card;
+import fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value;
 import fr.ninauve.renaud.kata.pokerhands.domain.model.Hand;
 import fr.ninauve.renaud.kata.pokerhands.domain.model.RankingResult;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FourOfAKindRanking implements FigureRanking {
 
   @Override
   public boolean matches(Hand hand) {
-    return false;
+    final Map<Value, Long> groupByValues =
+        hand.lowestToHighest().stream()
+            .collect(Collectors.groupingBy(Card::value, Collectors.counting()));
+    return groupByValues.containsValue(4L);
   }
 
   @Override
