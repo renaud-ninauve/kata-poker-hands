526dbcee0893d8fe6196026aece1b7e3c74e5ef3
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-06-18 15:47:54 +0200

    [red][red] shuffled hands are equals

---
+++ src/test/java/fr/ninauve/renaud/kata/pokerhands/domain/model/hand/HandTest.java
@@ -0,0 +1,34 @@
+package fr.ninauve.renaud.kata.pokerhands.domain.model.hand;
+
+import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Suit.DIAMONDS;
+import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Suit.SPADES;
+import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.FIVE;
+import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.FOUR;
+import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.SEVEN;
+import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.SIX;
+import static fr.ninauve.renaud.kata.pokerhands.domain.model.Card.Value.THREE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import fr.ninauve.renaud.kata.pokerhands.domain.model.Hand;
+import org.junit.jupiter.api.Test;
+
+class HandTest {
+
+  @Test
+  void shuffled_hands_are_equals() {
+    assertThat(
+            Hand.of(
+                THREE.of(DIAMONDS),
+                FOUR.of(SPADES),
+                FIVE.of(DIAMONDS),
+                SIX.of(SPADES),
+                SEVEN.of(DIAMONDS)))
+        .isEqualTo(
+            Hand.of(
+                FOUR.of(SPADES),
+                THREE.of(DIAMONDS),
+                FIVE.of(DIAMONDS),
+                SIX.of(SPADES),
+                SEVEN.of(DIAMONDS)));
+  }
+}
